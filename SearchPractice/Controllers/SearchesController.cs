4c2609876be5dcf368b2fe6c9242a684095f349c
Afrida07 <45858336+Afrida07@users.noreply.github.com>
2019-03-18 23:28:19 +0600

    Initial commit

---
+++ SearchPractice/Controllers/SearchesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SearchPractice.Models;
+
+namespace SearchPractice.Controllers
+{
+    public class SearchesController : Controller
+    {
+        private StudyAbroadEntities db = new StudyAbroadEntities();
+
+        // GET: Searches
+        public ActionResult Index()
+        {
+            return View(db.Searches.ToList());
+        }
+
+        // GET: Searches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Search search = db.Searches.Find(id);
+            if (search == null)
+            {
+                return HttpNotFound();
+            }
+            return View(search);
+        }
+
+        // GET: Searches/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Searches/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,UserName,Gender,Email")] Search search)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Searches.Add(search);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(search);
+        }
+
+        // GET: Searches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Search search = db.Searches.Find(id);
+            if (search == null)
+            {
+                return HttpNotFound();
+            }
+            return View(search);
+        }
+
+        // POST: Searches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,UserName,Gender,Email")] Search search)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(search).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(search);
+        }
+
+        // GET: Searches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Search search = db.Searches.Find(id);
+            if (search == null)
+            {
+                return HttpNotFound();
+            }
+            return View(search);
+        }
+
+        // POST: Searches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Search search = db.Searches.Find(id);
+            db.Searches.Remove(search);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
